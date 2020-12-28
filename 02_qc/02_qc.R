e8e736e12abbd557ef50bcd9b21d0fdd9bd280eb
Anastasiia Hryhorzhevska <ahryhorzhevska@slurmgate.psych.mpg.de>
2020-12-28 12:36:06 +0100

    pvalue chart added

--- 02_qc/02_qc.R
+++ 02_qc/02_qc.R
@@ -15,41 +15,41 @@ sh.script.mkdir <- sprintf("cd %s ;
 
 system(sh.script.mkdir)
 
-rgSet.fn     <- paste0(src.data.dir, "rgSet_dex.RData")
-
+packages.fn     <- paste0(src.dir, "packages.R")
 source(packages.fn)
-
-load(rgSet.fn)
-
 
 sample.sheet.fn <- paste0(src.dir, "samplesheet_epic_methylation_dex.csv")
 targets         <- read.csv(sample.sheet.fn, sep = ',' )
 
+rgSet.fn        <- paste0(src.data.dir, "rgSet_dex.RData")
+load(rgSet.fn)
 
 ##--- Calculate the detection p-values
 
-detP <- detectionP(rgSet)
-head(detP)
-save(detP, file = paste0(src.data.dir, "detP.Rdata")) 
-
 pvalues.fn <- paste0(src.data.dir, "detP.Rdata")
+detP       <- detectionP(rgSet)
+head(detP)
+save(detP, file = paste0(src.data.dir, "detP.Rdata")
+
 load(pvalues.fn)
 
 ##--- Examine mean detection p-values across all samples to identify any failed samples
 
-pdf(paste(report.dir, "detectionP.pdf", sep="/"), width = 8, height = 3)
-pal <- brewer.pal(8,"Dark2")
+pdf(paste(report.dir, "detectionP.pdf", sep="/"), paper = "a4r", width = 11, height = 8)
+pal <- brewer.pal(12,"Set3")
 par(mfrow = c(1,2))
 
 barplot(colMeans(detP), 
-	col = pal[factor(targets$Sample_Group)], 
-	las = 2, cex.names = 0.8, ylab = "Mean detection p-values")
-abline(h = 0.05, col = "red")
+	col = pal[factor(targets$BeadChipPosition)], 
+	las = 2, cex.names = 0.2, ylab = "Mean detection p-values")
+abline(h = 0.01, col = "red")
 legend("topleft", 
-	legend=levels(factor(targets$SampleName)), fill=pal, bg="white")
+	legend=levels(factor(targets$BeadChipPosition)), fill = pal, bg = "white")
 
 barplot(colMeans(detP), 
 	col = pal[factor(targets$Sample_Group)], 
 	las = 2, cex.names = 0.8, ylim = c(0,0.002), ylab = "Mean detection p-values")
-abline(h = 0.05, col = "red")
-legend("topleft", legend = levels(factor(targets$SampleName)), fill = pal, bg = "white")
+abline(h = 0.01, col = "red")
+legend("topleft", legend = levels(factor(targets$BeadChipPosition)), fill = pal, bg = "white")
+
+dev.off()
