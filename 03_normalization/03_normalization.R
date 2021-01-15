8e7877011f1d74e025ba029c51089cdbe2884cf8
anastasiiainna <anastasiia_hry@PCR183.fritz.box>
2021-01-15 20:20:42 +0100

    normalisation started

---
+++ 03_normalization/03_normalization.R
@@ -0,0 +1,21 @@
+# 03_normalisation
+
+args                <- commandArgs(T)
+input.parameters.fn <- as.character(args[1])
+
+input.parameters    <- read.csv(input.parameters.fn, sep = "\t", header = F )
+input.parameters    <- as.data.frame(input.parameters)
+
+for (row in 1:nrow(input.parameters))
+  assign(input.parameters[row, 1], input.parameters[row, 2])
+
+source(packages.fn)
+
+load(rgSet_clean.fn)
+# load(detP_clean.fn)
+# load(rawBetas_clean.fn)
+# load(pd_clean.fn)
+# load(annotated_data_clean.fn)
+
+quantileN <- preprocessQuantile(rgSet_clean)
+save(quantileN, file = quantileN.fn)
