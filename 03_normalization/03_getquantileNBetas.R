ffbb98eefb12acbd6b2fd2730557d7b80438fa6b
Anastasiia Hryhorzhevska <ahryhorzhevska@slurmgate.psych.mpg.de>
2021-01-17 15:22:42 +0100

    normalizarion done

--- 03_normalization/03_getquantileNBetas.R
+++ 03_normalization/03_getquantileNBetas.R
@@ -36,7 +36,7 @@ save(BMIQ.quantileN, file = quantileN.bmiq.fn)
 
 ## Check distributions before and after chosen normalization
 png(file = paste0(report.dir, "BetaValue_Distributions_Norm_Quantile.png"), width = 1400, height = 700, pointsize = 12)
-par(mfrow = c(1,2))
+par(mfrow = c(1,3))
 densityPlot(RawBetas_clean, sampGroups = pd_clean$Slide, legend=FALSE, main = "Raw Betas", xlab = "Beta")
 densityPlot(quantileNBetas, sampGroups = pd_clean$Slide, legend=FALSE, main = "Quantile Adjusted Betas", xlab = "Beta")
 densityPlot(BMIQ.quantileN, sampGroups = pd_clean$Slide, legend=FALSE, main = "Quantile-BMIQ Adjusted Betas", xlab = "Beta", ylim=c(0,5))
@@ -47,7 +47,7 @@ dev.off()
 ##  I) raw batas
 
  names_RawBetas <- colnames(RawBetas_clean)
- pdf(paste0(report.dir, Beta_Densities_RawBetas.pdf"))
+ pdf(paste0(report.dir, "Beta_Densities_RawBetas.pdf"))
  for (i in 1:ncol(RawBetas_clean)) {
    i_mat <- as.matrix(RawBetas_clean[ ,i])
    densityPlot(i_mat, main=names_RawBetas[i])
